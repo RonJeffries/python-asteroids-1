e3751613ba6cd944ea5abd5027d31bc3762f9b45
Ron Jeffries <ronjeffries@acm.org>
2023-03-24 14:44:28 -0400

    Big ship on black via blit.

--- ship.py
+++ ship.py
@@ -7,12 +7,17 @@ class Ship():
     def __init__(self, position):
         self.position = position
         self.raw_points = [vector2(-3.0, -2.0), vector2(-3.0, 2.0), vector2(-5.0, 4.0),
-                      vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
+                           vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
         self.raw_flare = [vector2(-3.0, -2.0), vector2(-7.0, 0.0), vector2(-3.0, 2.0)]
+        self.surface = pygame.Surface((60, 36))
+        self.paint(self.surface)
 
     def adjust(self, point):
-        return point + vector2(7, 4) + self.position
+        return point*4 + vector2(28, 16)
 
     def draw(self, screen):
+        screen.blit(self.surface, self.position)
+
+    def paint(self, surface):
         ship_points = list(map(self.adjust, self.raw_points))
-        pygame.draw.lines(screen, "white", False, ship_points)
+        pygame.draw.lines(surface, "white", False, ship_points)
