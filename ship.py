c6ea68fae3b43d236aaa49c039559d54a22f73fa
Ron Jeffries <ronjeffries@acm.org>
2023-03-25 08:51:00 -0400

    tidying Ship.

--- ship.py
+++ ship.py
@@ -9,30 +9,30 @@ class Ship:
         self.position = position
         self.angle = 0
         self.accelerating = False
-        self.raw_points = [vector2(-3.0, -2.0), vector2(-3.0, 2.0), vector2(-5.0, 4.0),
-                           vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
+        self.raw_ship = [vector2(-3.0, -2.0), vector2(-3.0, 2.0), vector2(-5.0, 4.0),
+                         vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
         self.raw_flare = [vector2(-3.0, -2.0), vector2(-7.0, 0.0), vector2(-3.0, 2.0)]
-        self.surface = pygame.Surface((60, 36))
-        self.surface.set_colorkey((0, 0, 0))
-        self.flare_surface = pygame.Surface((60, 36))
-        self.flare_surface.set_colorkey((0, 0, 0))
-        self.paint(self.surface, self.flare_surface)
+        self.ship_surface = pygame.Surface((60, 36))
+        self.ship_surface.set_colorkey((0, 0, 0))
+        self.ship_accelerating_surface = pygame.Surface((60, 36))
+        self.ship_accelerating_surface.set_colorkey((0, 0, 0))
+        self.paint()
 
     def adjust(self, point):
         return point*4 + vector2(28, 16)
 
     def draw(self, screen):
         if not self.accelerating or random.random() < 0.66:
-            ship_source = self.surface
+            ship_source = self.ship_surface
         else:
-            ship_source = self.flare_surface
+            ship_source = self.ship_accelerating_surface
         copy = pygame.transform.rotate(ship_source.copy(), self.angle)
         half = pygame.Vector2(copy.get_size())/2
         screen.blit(copy, self.position - half)
 
-    def paint(self, surface, flare_surface):
-        ship_points = list(map(self.adjust, self.raw_points))
+    def paint(self):
+        ship_points = list(map(self.adjust, self.raw_ship))
         flare_points = list(map(self.adjust, self.raw_flare))
-        pygame.draw.lines(surface, "white", False, ship_points, 3)
-        pygame.draw.lines(flare_surface, "white", False, ship_points, 3)
-        pygame.draw.lines(flare_surface, "white", False, flare_points, 3)
+        pygame.draw.lines(self.ship_surface, "white", False, ship_points, 3)
+        pygame.draw.lines(self.ship_accelerating_surface, "white", False, ship_points, 3)
+        pygame.draw.lines(self.ship_accelerating_surface, "white", False, flare_points, 3)
