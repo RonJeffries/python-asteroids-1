e27c3a3cd5ee956d187ba59d9450f6589f78e0ba
Ron Jeffries <ronjeffries@acm.org>
2023-03-25 09:13:55 -0400

    tidying Ship.

--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship:
     def __init__(self, position):
         self.position = position
         self.angle = 0
-        self.accelerating = False
+        self.accelerating = True
         self.raw_ship = [vector2(-3.0, -2.0), vector2(-3.0, 2.0), vector2(-5.0, 4.0),
                          vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
         self.raw_flare = [vector2(-3.0, -2.0), vector2(-7.0, 0.0), vector2(-3.0, 2.0)]
@@ -22,13 +22,16 @@ class Ship:
         return point*4 + vector2(28, 16)
 
     def draw(self, screen):
-        if not self.accelerating or random.random() < 0.66:
-            ship_source = self.ship_surface
-        else:
-            ship_source = self.ship_accelerating_surface
+        ship_source = self.select_ship_source()
         copy = pygame.transform.rotate(ship_source.copy(), self.angle)
         half = pygame.Vector2(copy.get_size())/2
         screen.blit(copy, self.position - half)
+
+    def select_ship_source(self):
+        if self.accelerating and random.random() >= 0.66:
+            return self.ship_accelerating_surface
+        else:
+            return self.ship_surface
 
     def paint(self):
         ship_points = list(map(self.adjust, self.raw_ship))
