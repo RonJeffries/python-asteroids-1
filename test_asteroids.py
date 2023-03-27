2464f9839102e93b1fa19d0e7e5ab8592aa55b37
Ron Jeffries <ronjeffries@acm.org>
2023-03-27 10:24:09 -0400

    converting to pytest

--- test_asteroids.py
+++ test_asteroids.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pygame
+import pytest
 
 from ship import Ship
 
@@ -9,35 +10,35 @@ vector2 = pygame.Vector2
 
 class AsteroidsTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+        assert True == True
 
     def test_map_lambda(self):
         points = [vector2(-3.0, -2.0), vector2(-3.0, 2.0), vector2(-5.0, 4.0),
                   vector2(7.0, 0.0), vector2(-5.0, -4.0), vector2(-3.0, -2.0)]
         new_points = map(lambda point: point + vector2(7, 4), points)
         for point in new_points:
-            self.assertGreaterEqual(point.x, 0)
-            self.assertLessEqual(point.x, 14)
-            self.assertGreaterEqual(point.y, 0)
-            self.assertLessEqual(point.y, 9)
+            assert point.x >= 0
+            assert point.x <= 14
+            assert point.y >= 0
+            assert point.y <= 9
 
     def test_ship_move(self):
         ship = Ship(vector2(50, 60))
         ship.velocity = vector2(10, 16)
         ship.move(0.5)
-        self.assertEqual(ship.position, vector2(55, 68))
+        assert ship.position == vector2(55, 68)
 
     def test_ship_acceleration(self):
         ship = Ship(vector2(0, 0))
         ship.angle = 45
         ship.acceleration = pygame.Vector2(100, 0)
         ship.power_on(0.5)
-        self.assertAlmostEqual(ship.velocity.x, 35.3553, 2, "x value")
-        self.assertAlmostEqual(ship.velocity.y, -35.3553, 2, "y value")
+        assert ship.velocity.x == pytest.approx(35.3553, 0.01)
+        assert ship.velocity.y == pytest.approx(-35.3553, 0.01)
 
     def test_mod(self):
-        self.assertEqual(1005 % 1000, 5)
-        self.assertEqual(-5 % 1000, 995)
+        assert 1005 % 1000 == 5
+        assert -5 % 1000 == 995
 
 
 if __name__ == '__main__':
