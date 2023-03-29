e7e0b602b5a599ae9a4b42d0198658024ff3a713
Ron Jeffries <ronjeffries@acm.org>
2023-03-29 17:06:38 -0400

    refactoring SurfaceMaker

--- SurfaceMaker.py
+++ SurfaceMaker.py
@@ -1,6 +1,10 @@
 import pygame
 from pygame import Vector2
 
+raw_ship_points = [Vector2(-3.0, -2.0), Vector2(-3.0, 2.0), Vector2(-5.0, 4.0),
+                   Vector2(7.0, 0.0), Vector2(-5.0, -4.0), Vector2(-3.0, -2.0)]
+raw_flare_points = [Vector2(-3.0, -2.0), Vector2(-7.0, 0.0), Vector2(-3.0, 2.0)]
+
 raw_rocks = [
     [
         Vector2(4.0, 2.0), Vector2(3.0, 0.0), Vector2(4.0, -2.0),
@@ -33,36 +37,15 @@ raw_rocks = [
 
 class SurfaceMaker:
     def __init__(self):
-        self._two = 2
         pass
 
     def adjust(self, point, center_adjustment, scale_factor):
         return (point + center_adjustment) * scale_factor
 
-    def get_ship_points(self):
-        ship_points = [Vector2(-3.0, -2.0), Vector2(-3.0, 2.0), Vector2(-5.0, 4.0),
-                       Vector2(7.0, 0.0), Vector2(-5.0, -4.0), Vector2(-3.0, -2.0)]
-        return [self.adjust(point, Vector2(7, 4), 4) for point in ship_points]
-
-    def get_flare_points(self):
-        flare_points = [Vector2(-3.0, -2.0), Vector2(-7.0, 0.0), Vector2(-3.0, 2.0)]
-        return [self.adjust(point, Vector2(7, 4), 4) for point in flare_points]
-
-    def make_ship_surface(self, ship_points):
-        ship_surface = pygame.Surface((60, 36))
-        ship_surface.set_colorkey((0, 0, 0))
-        pygame.draw.lines(ship_surface, "white", False, ship_points, 3)
-        return ship_surface
-
-    def make_accelerating_surface(self, flare_points, ship_points):
-        ship_accelerating_surface = self.make_ship_surface(ship_points)
-        pygame.draw.lines(ship_accelerating_surface, "white", False, flare_points, 3)
-        return ship_accelerating_surface
-
-    def prepare_surfaces(self):
-        ship_points = self.get_ship_points()
-        flare_points = self.get_flare_points()
-        return (self.make_ship_surface(ship_points)), (self.make_accelerating_surface(flare_points, ship_points))
+    def ship_surfaces(self):
+        ship_surface = self.create_scaled_surface((60, 36), Vector2(7, 4), 4, raw_ship_points)
+        accelerating_surface = self.create_scaled_surface((60, 36), Vector2(7, 4), 4, raw_ship_points, raw_flare_points)
+        return ship_surface, accelerating_surface
 
     def asteroid_surface(self, shape, size):
         return self.create_scaled_surface((128, 128), Vector2(4, 4), 16, raw_rocks[0])
