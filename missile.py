562b1e4f8094df216a08bf8fff98e5687103f74c
Ron Jeffries <ronjeffries@acm.org>
2023-04-04 08:50:38 -0400

    Missile is its own mover.

--- missile.py
+++ missile.py
@@ -1,17 +1,27 @@
 # Missile
 
 import pygame
+
+import u
 from mover import Mover
 
 
 class Missile:
     def __init__(self, position, velocity):
+        self.position = position.copy()
+        self.velocity = velocity.copy()
         self.radius = 2
-        self.mover = Mover(position, velocity)
+        self.mover = self
         self.time = 0
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.mover.position, 4)
+
+    def move(self, deltaTime):
+        position = self.position + self.velocity * deltaTime
+        position.x = position.x % u.SCREEN_SIZE
+        position.y = position.y % u.SCREEN_SIZE
+        self.position = position
 
     def update(self, missiles, delta_time):
         self.time += delta_time
