2af4dca80bf1fa8e37440aeca36e9032bbd7c956
Ron Jeffries <ronjeffries@acm.org>
2023-04-30 09:44:42 -0400

    individual collections are virtual in SpaceObjects.

--- spaceobjects.py
+++ spaceobjects.py
@@ -2,10 +2,25 @@
 
 class SpaceObjects:
     def __init__(self, asteroids, missiles, saucers, saucer_missiles, ships):
-        self.asteroids = asteroids
-        self.missiles = missiles
-        self.saucers = saucers
-        self.saucer_missiles = saucer_missiles
-        self.ships = ships
         self.collections = (asteroids, missiles, saucers, saucer_missiles, ships)
+
+    @property
+    def asteroids(self):
+        return self.collections[0]
+
+    @property
+    def missiles(self):
+        return self.collections[1]
+
+    @property
+    def saucers(self):
+        return self.collections[2]
+
+    @property
+    def saucer_missiles(self):
+        return self.collections[3]
+
+    @property
+    def ships(self):
+        return self.collections[4]
 
