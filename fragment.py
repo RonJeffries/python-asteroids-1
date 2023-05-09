ce1f6916a7573542a236ee2f302eb78597b71159
Ron Jeffries <ronjeffries@acm.org>
2023-05-09 10:56:35 -0400

    fragment times out after u.FRAGMENT_LIFETIME.

--- fragment.py
+++ fragment.py
@@ -1,5 +1,20 @@
+import random
+
+from pygame import Vector2
+
+import u
+from timer import Timer
 
 
 class Fragment():
-    def __init__(self):
-        pass
+    def __init__(self, angle=None):
+        angle = angle if angle else random.randrange(360)
+        self.velocity = Vector2(u.FRAGMENT_SPEED, 0).rotate(angle)
+        self.timer = Timer(u.FRAGMENT_LIFETIME, self.timeout)
+
+    def tick(self, delta_time, fragments, _fleets):
+        self.timer.tick(delta_time, fragments)
+        # self.move(delta_time)
+
+    def timeout(self, fragments):
+        fragments.remove(self)
--- test_frament.py
+++ test_frament.py
@@ -1,3 +1,8 @@
+from pygame import Vector2
+
+import u
+from fleet import ExplosionFleet
+from fleets import Fleets
 from fragment import Fragment
 
 
@@ -5,3 +10,19 @@ class TestFragments():
     def test_frag(self):
         frag = Fragment()
         assert frag
+
+    def test_frag_random_angle(self):
+        angle = 180
+        frag = Fragment(angle=angle)
+        assert frag.velocity == Vector2(u.FRAGMENT_SPEED, 0).rotate(angle)
+        assert frag.velocity.x == -u.FRAGMENT_SPEED
+
+    def test_frag_timeout(self):
+        frag = Fragment()
+        fleets = Fleets()
+        frags = fleets.explosions
+        frags.append(frag)
+        frags.tick(0.1, fleets)
+        assert frags
+        frags.tick(u.FRAGMENT_LIFETIME, fleets)
+        assert not frags
