acd0427eaab756fd266a81e808f74b76a5fd370e
Ron Jeffries <ronjeffries@acm.org>
2023-05-10 09:27:38 -0400

    G Fragment done and working. Code a bit naff.

--- fragment.py
+++ fragment.py
@@ -23,7 +23,7 @@ class Fragment:
     def draw(self, screen):
         begin = self.position + self.begin.rotate(self.theta)
         end = self.position + self.end.rotate(self.theta)
-        pygame.draw.line(screen, "red", begin, end, 3)
+        pygame.draw.line(screen, "white", begin, end, 3)
 
     def move(self, delta_time):
         position = self.position + self.velocity * delta_time
@@ -43,6 +43,25 @@ class VFragment(Fragment):
     def __init__(self, position, angle=None, speed_mul=None):
         super().__init__(position, angle, speed_mul)
 
+    def draw(self, screen):
+        v_shape = [Vector2(-7, 5), Vector2(7, 0), Vector2(-7, -5)]
+        points = [p.rotate(self.theta) + self.position for p in v_shape]
+        pygame.draw.lines(screen, "white", False, points, 3)
+
 class GFragment(Fragment):
     def __init__(self, position, angle=None, speed_mul=None):
         super().__init__(position, angle, speed_mul)
+
+    def draw(self, screen):
+        head_off = Vector2(0,16+8).rotate(self.theta)
+        pygame.draw.circle(screen, "white", self.position + head_off, 8, 2)
+        body_top = Vector2(0, 16).rotate(self.theta)
+        body_bottom = Vector2(0, 2).rotate(self.theta)
+        pygame.draw.line(screen, "white", body_top+self.position, body_bottom+self.position, 3)
+        leg_left = Vector2(-5, -16).rotate(self.theta)
+        pygame.draw.line(screen, "white", leg_left+self.position, body_bottom+self.position, 3)
+        leg_right = Vector2(5, -16).rotate(self.theta)
+        pygame.draw.line(screen, "white", leg_right+self.position, body_bottom+self.position, 3)
+        arm_left = Vector2(-9, 10).rotate(self.theta)
+        arm_right = Vector2(9, 10).rotate(self.theta)
+        pygame.draw.line(screen, "white", arm_right+self.position, arm_left+self.position, 3)
