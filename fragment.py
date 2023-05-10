be59a6f6c2c98e44e260d366c30873490137011e
Ron Jeffries <ronjeffries@acm.org>
2023-05-10 16:11:59 -0400

    move frag creation down.

--- fragment.py
+++ fragment.py
@@ -11,14 +11,14 @@ class Fragment:
     def __init__(self, position, angle=None, speed_mul=None):
         angle = angle if angle is not None else random.randrange(360)
         self.position = position
-        half_length = random.uniform(6, 10)
-        self.begin = Vector2(-half_length, 0)
-        self.end = Vector2(half_length, 0)
         speed_mul = speed_mul if speed_mul is not None else random.uniform(0.25, 0.5)
         self.velocity = speed_mul*Vector2(u.FRAGMENT_SPEED, 0).rotate(angle)
         self.theta = random.randrange(0, 360)
         self.delta_theta = random.uniform(180, 360)*random.choice((1, -1))
         self.timer = Timer(u.FRAGMENT_LIFETIME, self.timeout)
+        half_length = random.uniform(6, 10)
+        self.begin = Vector2(-half_length, 0)
+        self.end = Vector2(half_length, 0)
 
     def draw(self, screen):
         frags = [[self.begin, self.end]]
