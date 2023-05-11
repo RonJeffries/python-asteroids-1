ce0e67d4c03e44d5be843aca1a59544525d85824
Ron Jeffries <ronjeffries@acm.org>
2023-05-11 16:24:35 -0400

    move implemented with wrap.

--- movable_position.py
+++ movable_position.py
@@ -1,9 +1,15 @@
+import u
 
 
-class MovablePosition:
-    def __init__(self, position, velocity):
+class MovableLocation:
+    def __init__(self, position, velocity, size=u.SCREEN_SIZE):
         self.position = position
         self.velocity = velocity
+        self.size = size
 
     def move(self, delta_time):
-        self.position += self.velocity*delta_time
+        position = self.position + self.velocity*delta_time
+        position.x = position.x % self.size
+        position.y = position.y % self.size
+        self.position = position
+
--- test_movable_position.py
+++ test_movable_position.py
@@ -1,17 +1,24 @@
 from pygame import Vector2
 
-from movable_position import MovablePosition
+from movable_position import MovableLocation
 
 
 class TestMovablePosition:
     def test_creation(self):
         position = Vector2(0, 0)
         velocity = Vector2(100, 200)
-        mp = MovablePosition(position, velocity)
+        mp = MovableLocation(position, velocity)
 
     def test_motion(self):
         position = Vector2(0, 0)
         velocity = Vector2(100, 200)
-        mp = MovablePosition(position, velocity)
+        mp = MovableLocation(position, velocity)
         mp.move(0.25)
         assert mp.position == Vector2(25, 50)
+
+    def test_motion_wraps(self):
+        position = Vector2(990, 990)
+        velocity = Vector2(100, 200)
+        mp = MovableLocation(position, velocity, 1000)
+        mp.move(0.25)
+        assert mp.position == Vector2(15, 40)
