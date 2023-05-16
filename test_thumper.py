3c842b920d4029ca257b13cad08ccbc5779aae0f
Ron Jeffries <ronjeffries@acm.org>
2023-05-16 07:39:11 -0400

    Thumper class rebuilt to know its limits of operation.

---
+++ test_thumper.py
@@ -0,0 +1,50 @@
+from thumper import Thumper
+
+thump = None
+
+
+def beat1():
+    global thump
+    thump = 1
+
+
+def beat2():
+    global thump
+    thump = 2
+
+
+class TestThumper:
+    def test_thumper_create(self):
+        thumper = Thumper(beat1, beat2)
+        assert thumper
+        assert thumper._long_interval == 30/60
+        assert thumper._short_interval == 8/60
+
+    def test_thumper_thumps(self):
+        """thumper ticks between 30 and 8 60ths"""
+        thumper = Thumper(beat1, beat2)
+        assert not thump
+        thumper.tick(31/60)
+        assert thump == 1
+        thumper.tick(31/60)
+        assert thump == 2
+        thumper.tick(31/60)
+        assert thump == 1
+
+    def test_thumper_decrement_interval(self):
+        thumper = Thumper(beat1, beat2)
+        thumper.tick(128/60)
+        assert thumper._interval == 30/60 - 1/60
+        thumper.tick(128/60)
+        assert thumper._interval == 30/60 - 1/60 - 1/60
+
+    def test_thumper_countdown(self):
+        thumper = Thumper(beat1, beat2)
+        thumper._interval = 9/60
+        thumper.tick(128/60)
+        assert thumper._interval == 9/60 - 1/60
+        thumper.tick(128/60)
+        assert thumper._interval == 8/60
+        thumper.tick(128/60)
+        assert thumper._interval == 8/60
+
---
+++ thumper.py
@@ -0,0 +1,25 @@
+
+
+class Thumper:
+    def __init__(self, b1, b2):
+        self._long_interval = 30/60
+        self._short_interval = 8/60
+        self._interval = self._long_interval
+        self._decrement_interval = 127/60
+        self._decrement_time = 0
+        self._execute_time = 0
+        self.b1 = b1
+        self.b2 = b2
+
+    def tick(self, delta_time):
+        self._execute_time += delta_time
+        if self._execute_time >= self._interval:
+            self._execute_time = 0
+            self.b1()
+            self.b1, self.b2 = self.b2, self.b1
+        self._decrement_time += delta_time
+        if self._decrement_time >= self._decrement_interval:
+            self._decrement_time = 0
+            self._interval = self._interval - 1/60
+            if self._interval < 8/60:
+                self._interval = 8/60
