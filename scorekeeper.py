5e0e0beb13055a49c0e66df4bafd1703e46012a0
Ron Jeffries <ronjeffries@acm.org>
2023-05-22 09:12:59 -0400

    remove default interact_with methods.

--- scorekeeper.py
+++ scorekeeper.py
@@ -14,23 +14,8 @@ class ScoreKeeper(Flyer):
     def interact_with(self, other, fleets):
         other.interact_with_scorekeeper(self, fleets)
 
-    def interact_with_asteroid(self, asteroid, fleets):
-        pass
-
-    def interact_with_missile(self, missile, fleets):
-        pass
-
-    def interact_with_saucer(self, saucer, fleets):
-        pass
-
     def interact_with_score(self, score, fleets):
         self.score += score.score
-
-    def interact_with_scorekeeper(self, scorekeeper, fleets):
-        pass
-
-    def interact_with_ship(self, ship, fleets):
-        pass
 
     def tick(self, delta_time, _fleet, _fleets):
         pass
