e2864d3ea325d4e7eba4fd1b46b0e18d9a5f535d
Ron Jeffries <ronjeffries@acm.org>
2023-05-23 08:31:32 -0400

    moved to own file

---
+++ wavemaker.py
@@ -0,0 +1,34 @@
+import u
+from asteroid import Asteroid
+from flyer import Flyer
+from timer import Timer
+
+
+class WaveMaker(Flyer):
+    def __init__(self):
+        self.saw_asteroids = None
+        self.timer = Timer(u.ASTEROID_DELAY, self.create_asteroids)
+        self.asteroid_count = 2
+
+    def create_asteroids(self, fleets):
+        self.asteroid_count += 2
+        if self.asteroid_count > 11:
+            self.asteroid_count = 11
+        for i in range(self.asteroid_count):
+            fleets.add_asteroid(Asteroid())
+
+    def begin_interactions(self, fleets):
+        self.saw_asteroids = False
+
+    def interact_with_asteroid(self, asteroid, fleets):
+        self.saw_asteroids = True
+
+    def interact_with(self, other, fleets):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def tick(self, delta_time, fleet, fleets):
+        if not self.saw_asteroids:
+            self.timer.tick(delta_time, fleets)
