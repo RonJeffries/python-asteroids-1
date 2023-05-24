848222dc16e7f67d1f3b75100722661c86d4a067
Ron Jeffries <ronjeffries@acm.org>
2023-05-24 08:12:29 -0400

    Initial FireControl and test.

---
+++ test_fire_control.py
@@ -0,0 +1,11 @@
+
+
+class FireControl:
+    def __init__(self, number_of_missiles):
+        self.number_of_missiles = number_of_missiles
+
+
+class TestFireControl:
+    def test_class(self):
+        FireControl(4)
+
