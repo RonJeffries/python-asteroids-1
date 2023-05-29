6ef25cbf767d2348c1669eaf4159962401cd7656
Ron Jeffries <ronjeffries@acm.org>
2023-05-29 07:22:33 -0400

    Initial ShipMaker class.

---
+++ shipmaker.py
@@ -0,0 +1,12 @@
+from flyer import Flyer
+
+
+class ShipMaker(Flyer):
+    def interact_with(self, other, fleets):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def tick(self, delta_time, fleet, fleets):
+        pass
---
+++ test_shipmaker.py
@@ -0,0 +1,6 @@
+from shipmaker import ShipMaker
+
+
+class TestShipMaker:
+    def test_exists(self):
+        ShipMaker()
