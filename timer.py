e97845565212d6fd6cf2f19c5e7636b4c90a3416
Ron Jeffries <ronjeffries@acm.org>
2023-06-02 11:14:05 -0400

    Timer now expects action callback in `tick` and so specifies. General refactoring to simplify.

--- timer.py
+++ timer.py
@@ -4,21 +4,14 @@ from typing import Callable, Union
 
 class Timer:
     def __init__(self, delay):
-        """action is callable returning None or bool"""
         self.delay = delay
-        self.action = self.call_back
         self.elapsed = 0
 
-    def tick(self, delta_time, *tick_args):
+    def tick(self, delta_time, action: Callable[[...], Union[None, bool]], *args):
         self.elapsed += delta_time
         if self.elapsed >= self.delay:
-            action = self.call_back
-            action_complete = action(*tick_args)
+            action_complete = action(*args)
             if action_complete is None or action_complete:
                 self.elapsed = 0
-
-    @staticmethod
-    def call_back(action: Callable[[...], Union[None, bool]], *args):
-        return action(*args)
 
 
