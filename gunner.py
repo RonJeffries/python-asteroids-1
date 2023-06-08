669cb74f4295532548b7cb74ead22ac144f4c089
Ron Jeffries <ronjeffries@acm.org>
2023-06-08 08:41:19 -0400

    Gunner improving.

--- gunner.py
+++ gunner.py
@@ -1,3 +1,16 @@
+from pygame import Vector2
+
+import u
+from missile import Missile
+from timer import Timer
+
 
 class Gunner:
-    pass
+    def __init__(self):
+        self._timer = Timer(u.SAUCER_MISSILE_DELAY)
+
+    def fire(self, delta_time, saucer_position, ship_position, fleets):
+        self._timer.tick(delta_time, self.fire_missile, saucer_position, ship_position, fleets)
+
+    def fire_missile(self, saucer_position, ship_position, fleets):
+        fleets.add_flyer(Missile.from_saucer(saucer_position, Vector2(0, 0)))
--- test_gunner.py
+++ test_gunner.py
@@ -1,6 +1,27 @@
+from pygame import Vector2
+
+import u
+from fleets import Fleets
 from gunner import Gunner
+from test_interactions import FI
 
 
 class TestGunner:
     def test_exists(self):
-        Gunner()
+        assert Gunner()
+
+    def test_no_fire(self):
+        delta_time = 0.1
+        saucer_position = Vector2(0, 0 )
+        ship_position = Vector2(1, 1)
+        fleets = Fleets()
+        Gunner().fire(delta_time, saucer_position, ship_position, fleets)
+        assert not FI(fleets).saucer_missiles
+
+    def test_fire(self):
+        delta_time = u.SAUCER_MISSILE_DELAY
+        saucer_position = Vector2(0, 0 )
+        ship_position = Vector2(1, 1)
+        fleets = Fleets()
+        Gunner().fire(delta_time, saucer_position, ship_position, fleets)
+        assert FI(fleets).saucer_missiles
