a474391cd2fa53880a58129aaf01abfbca316833
Ron Jeffries <ronjeffries@acm.org>
2023-06-10 09:32:52 -0400

    tidying

--- gunner.py
+++ gunner.py
@@ -33,20 +33,24 @@ class Gunner:
         missile = self.missile_at_angle(saucer_position, random_angle*360.0, saucer_velocity)
         fleets.add_flyer(missile)
 
-    def create_targeted_missile(self, saucer_position, ship_position, fleets):
-        aiming_point = self.nearest_point(saucer_position, ship_position, u.SCREEN_SIZE)
-        angle = Vector2(0, 0).angle_to(aiming_point - saucer_position)
-        missile = self.missile_at_angle(saucer_position, angle, Vector2(0, 0))
+    def create_targeted_missile(self, from_position, to_position, fleets):
+        best_aiming_point = self.best_aiming_point(from_position, to_position, u.SCREEN_SIZE)
+        angle = self.angle_to_hit(best_aiming_point, from_position)
+        missile = self.missile_at_angle(from_position, angle, Vector2(0, 0))
         fleets.add_flyer(missile)
+
+    @staticmethod
+    def angle_to_hit(best_aiming_point, saucer_position):
+        return Vector2(0, 0).angle_to(best_aiming_point - saucer_position)
 
     def missile_at_angle(self, position, desired_angle, velocity_adjustment):
         missile_velocity = Vector2(u.MISSILE_SPEED, 0).rotate(desired_angle) + velocity_adjustment
         offset = Vector2(2 * self._radius, 0).rotate(desired_angle)
         return Missile.from_saucer(position + offset, missile_velocity)
 
-    def nearest_point(self, shooter, target, wrap_size):
-        nearest_x = self.nearest(shooter.x, target.x, wrap_size)
-        nearest_y = self.nearest(shooter.y, target.y, wrap_size)
+    def best_aiming_point(self, shooter_position, target_position, wrap_size):
+        nearest_x = self.nearest(shooter_position.x, target_position.x, wrap_size)
+        nearest_y = self.nearest(shooter_position.y, target_position.y, wrap_size)
         return Vector2(nearest_x, nearest_y)
 
     @staticmethod
