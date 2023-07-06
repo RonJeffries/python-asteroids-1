2a2d98a9b3da8b9e93b738cccf7d187d003c5a0b
Ron Jeffries <ronjeffries@acm.org>
2023-07-06 07:38:51 -0400

    move coin from class methods to top-level vanilla functions.

--- tests/test_coin.py
+++ tests/test_coin.py
@@ -1,6 +1,7 @@
 from pygame import Vector2
 
-from coin import Coin
+import coin
+from asteroid import Asteroid
 from explosion import Explosion
 from fleets import Fleets
 from tests.test_interactions import FI
@@ -11,32 +12,38 @@ class TestCoin:
     def test_slug(self):
         fleets = Fleets()
         fi = FI(fleets)
-        Coin.slug(fleets)
+        fleets.append(Asteroid())
+        coin.slug(fleets)
         assert fi.saucermakers
         assert fi.scorekeepers
         assert fi.thumpers
         assert fi.wavemakers
         assert not fi.shipmakers
+        assert not fi.asteroids
 
     def test_quarter(self):
         fleets = Fleets()
         fi = FI(fleets)
-        Coin.quarter(fleets)
+        fleets.append(Asteroid())
+        coin.quarter(fleets)
         assert fi.saucermakers
         assert fi.scorekeepers
         assert fi.thumpers
         assert fi.wavemakers
         assert fi.shipmakers
+        assert not fi.asteroids
 
     def test_no_asteroids(self):
         fleets = Fleets()
         fi = FI(fleets)
-        Coin.no_asteroids(fleets)
+        fleets.append(Asteroid())
+        coin.no_asteroids(fleets)
         assert fi.saucermakers
         assert fi.scorekeepers
         assert fi.thumpers
         assert not fi.wavemakers
         assert fi.shipmakers
+        assert not fi.asteroids
 
     def test_saucer_explosion(self):
         fleets = Fleets()
