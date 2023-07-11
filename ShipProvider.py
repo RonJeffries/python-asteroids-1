8f410ae647b59c820dd5a657064d55c46b5cce6c
Ron Jeffries <ronjeffries@acm.org>
2023-07-11 08:41:13 -0400

    TwoPlayerShipProvider progress.

--- ShipProvider.py
+++ ShipProvider.py
@@ -20,3 +20,31 @@ class SinglePlayerShipProvider:
             return [Ship(u.CENTER), Signal(u.PLAYER_ZERO)]
         else:
             return []
+
+class TwoPlayerShipProvider:
+    def __init__(self, number_of_ships):
+        self._current_player = 1
+        self._ships = [number_of_ships, number_of_ships]
+
+    def ships_available(self, player):
+        return self._ships[player]
+
+    def ship_for_player(self, player):
+        self._ships[player] -= 1
+        return [Ship(u.CENTER), Signal(player)]
+
+    def provide(self):
+        self.switch_players()
+        if self._ships[self._current_player]:
+            return self.ship_for_player(self._current_player)
+        else:
+            self.switch_players()
+            if self._ships[self._current_player]:
+                return self.ship_for_player(self._current_player)
+            else:
+                return []
+
+    def switch_players(self):
+        self._current_player = (self._current_player + 1) % 2
+
+
--- tests/test_ship_providers.py
+++ tests/test_ship_providers.py
@@ -1,5 +1,5 @@
 import u
-from ShipProvider import SinglePlayerShipProvider
+from ShipProvider import SinglePlayerShipProvider, TwoPlayerShipProvider
 from fleets import Fleets
 from ship import Ship
 from signal import Signal
@@ -24,4 +24,24 @@ class TestShipProviders:
         provider = SinglePlayerShipProvider(4)
         provider.add_ship(u.PLAYER_ZERO)
         assert provider.ships_available(u.PLAYER_ZERO) == 5
+
+    def test_two_player(self):
+        provider = TwoPlayerShipProvider(2)
+        assert provider.ships_available(u.PLAYER_ZERO) == 2
+        assert provider.ships_available(u.PLAYER_ONE) == 2
+
+    def test_one_ship_for_each(self):
+        provider = TwoPlayerShipProvider(1)
+        items = provider.provide()
+        ship = next(s for s in items if isinstance(s, Ship))
+        assert ship.position == u.CENTER
+        signal = next(s for s in items if isinstance(s, Signal))
+        assert signal.signal == u.PLAYER_ZERO
+        items = provider.provide()
+        ship = next(s for s in items if isinstance(s, Ship))
+        assert ship.position == u.CENTER
+        signal = next(s for s in items if isinstance(s, Signal))
+        assert signal.signal == u.PLAYER_ONE
+        assert not provider.ships_available(u.PLAYER_ZERO)
+        assert not provider.ships_available(u.PLAYER_ONE)
 
