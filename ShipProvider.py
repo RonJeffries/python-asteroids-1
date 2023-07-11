b62859b87a1cbfad39235fdcaf7a2dcd7cf8b1dc
Ron Jeffries <ronjeffries@acm.org>
2023-07-11 08:10:12 -0400

    SinglePlayerShipProvider ready to install.

--- ShipProvider.py
+++ ShipProvider.py
@@ -1,7 +1,22 @@
+import u
+from game_over import GameOver
+from ship import Ship
+from signal import Signal
 
-class SinglePlayerShipProvider():
+
+class SinglePlayerShipProvider:
     def __init__(self, number_of_ships):
         self._ships = number_of_ships
 
+    def add_ship(self, _player):
+        self._ships += 1
+
     def ships_available(self, _player):
         return self._ships
+
+    def provide(self):
+        if self._ships:
+            self._ships -= 1
+            return [Ship(u.CENTER), Signal(u.PLAYER_ZERO)]
+        else:
+            return []
--- tests/test_ship_providers.py
+++ tests/test_ship_providers.py
@@ -1,12 +1,27 @@
+import u
 from ShipProvider import SinglePlayerShipProvider
 from fleets import Fleets
+from ship import Ship
+from signal import Signal
 from tests.tools import FI
 
 
 class TestShipProviders:
     def test_single_provider(self):
-        fleets = Fleets()
-        fi = FI(fleets)
         provider = SinglePlayerShipProvider(4)
-        assert provider.ships_available(0) == 4
+        assert provider.ships_available(u.PLAYER_ZERO) == 4
+        for i in range(0, 4):
+            assert provider.ships_available(u.PLAYER_ZERO) == 4 - i
+            items = provider.provide()
+            ship = next(s for s in items if isinstance(s, Ship))
+            assert ship.position == u.CENTER
+            signal = next(s for s in items if isinstance(s, Signal))
+            assert signal.signal == u.PLAYER_ZERO
+        assert provider.ships_available(u.PLAYER_ZERO) == 0
+        assert not provider.provide()
+
+    def test_single_add(self):
+        provider = SinglePlayerShipProvider(4)
+        provider.add_ship(u.PLAYER_ZERO)
+        assert provider.ships_available(u.PLAYER_ZERO) == 5
 
