dadbd7bbad0eee4553ce71dbe1b4dbfef821a56a
Ron Jeffries <ronjeffries@acm.org>
2023-07-21 08:23:00 -0400

    extract points_to_draw

--- SurfaceMaker.py
+++ SurfaceMaker.py
@@ -91,13 +91,14 @@ class SurfaceMaker:
 
     @staticmethod
     def ship_surface(object_size: Vector2):
+        points_to_draw = raw_ship_points
         room_for_fat_line = Vector2(0, 0)
         raw_points_span = Vector2(14, 8)
         raw_points_offset = raw_points_span / 2
         scale_factor = object_size.x / raw_points_span.x
         expanded_size = object_size + room_for_fat_line
         ship_surface = SurfaceMaker.create_scaled_surface(
-            expanded_size, raw_points_offset, scale_factor, raw_ship_points)
+            expanded_size, raw_points_offset, scale_factor, points_to_draw)
         return ship_surface
 
     @staticmethod
