91a7e8660039a6edc55a4043afe4fe749c6ab868
Ron Jeffries <ronjeffries@acm.org>
2023-08-10 06:52:56 -0400

    invaders all display their first picture.

--- invader.py
+++ invader.py
@@ -6,6 +6,7 @@ INVADER_SPACING = 64
 
 class Invader:
     def __init__(self, x, y, bitmaps):
+        self.bitmaps = bitmaps
         self.relative_position = Vector2(INVADER_SPACING * x, -INVADER_SPACING * y)
         self.rect = pygame.Rect(0, 0, 64, 32)
 
@@ -18,9 +19,10 @@ class Invader:
 
     def draw(self, screen):
         if screen:
-            circle_color = "green" if self.relative_position == Vector2(0, 0) else "red"
-            pygame.draw.rect(screen, "yellow", self.rect)
-            pygame.draw.circle(screen, circle_color, self.rect.center, 16)
+            screen.blit(self.bitmaps[0], self.rect)
+            # circle_color = "green" if self.relative_position == Vector2(0, 0) else "red"
+            # pygame.draw.rect(screen, "yellow", self.rect)
+            # pygame.draw.circle(screen, circle_color, self.rect.center, 16)
 
     def interact_with_bumper(self, bumper, invader_fleet):
         if bumper.intersecting(self.rect):
