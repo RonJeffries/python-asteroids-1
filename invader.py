021ee2f391919862ea80baa752a2e75a4433daa7
Ron Jeffries <ronjeffries@acm.org>
2023-08-11 07:08:45 -0400

    create `column` member on invader.

--- invader.py
+++ invader.py
@@ -5,19 +5,16 @@ INVADER_SPACING = 64
 
 
 class Invader:
-    def __init__(self, x, y, bitmaps):
+    def __init__(self, column, row, bitmaps):
         self.bitmaps = bitmaps
-        self.relative_position = Vector2(INVADER_SPACING * x, -INVADER_SPACING * y)
+        self.column = column
+        self.relative_position = Vector2(INVADER_SPACING * column, -INVADER_SPACING * row)
         self.rect = pygame.Rect(0, 0, 64, 32)
         self.image = 0
 
     @property
     def position(self):
         return Vector2(self.rect.center)
-
-    @property
-    def column(self):
-        return self.relative_position.x // INVADER_SPACING
 
     def set_position(self, origin):
         self.image = 1 if self.image == 0 else 0
