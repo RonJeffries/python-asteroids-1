4961a0348d78b5e207696d96f9ff587e8c082cf9
Ron Jeffries <ronjeffries@acm.org>
2023-08-13 06:30:36 -0400

    player only fires if no shots on screen.

--- invader_player.py
+++ invader_player.py
@@ -8,14 +8,6 @@ from player_shot import PlayerShot
 
 
 class InvaderPlayer(InvadersFlyer):
-    def interact_with_invaderfleet(self, bumper, fleets):
-        pass
-
-    def interact_with_invaderplayer(self, bumper, fleets):
-        pass
-
-    def interact_with_playershot(self, bumper, fleets):
-        pass
 
     def __init__(self):
         maker = BitmapMaker.instance()
@@ -27,9 +19,17 @@ class InvaderPlayer(InvadersFlyer):
         half_width = self.rect.width / 2
         self.left = 64 + half_width
         self.right = 960 - half_width
+        self.free_to_fire = True
+
+    def begin_interactions(self, fleets):
+        self.free_to_fire = True
+
+    def interact_with_playershot(self, bumper, fleets):
+        self.free_to_fire = False
 
     def attempt_firing(self, fleets):
-        fleets.append(PlayerShot())
+        if self.free_to_fire:
+            fleets.append(PlayerShot())
 
     def update(self, _delta_time, _fleets):
         if not pygame.get_init():
@@ -46,11 +46,17 @@ class InvaderPlayer(InvadersFlyer):
     def interact_with(self, other, fleets):
         other.interact_with_invaderplayer(self, fleets)
 
+    def interact_with_bumper(self, bumper, fleets):
+        pass
+
+    def interact_with_invaderfleet(self, bumper, fleets):
+        pass
+
+    def interact_with_invaderplayer(self, bumper, fleets):
+        pass
+
     def draw(self, screen):
         screen.blit(self.player, self.rect)
 
     def tick(self, delta_time, fleets):
-        pass
-
-    def interact_with_bumper(self, bumper, fleets):
         pass
--- tests/test_player.py
+++ tests/test_player.py
@@ -21,3 +21,18 @@ class TestPlayer:
         fleets.append(player)
         player.attempt_firing(fleets)
         assert fi.player_shots
+
+    def test_cannot_fire_with_one_on_screen(self):
+        fleets = Fleets()
+        fi = FI(fleets)
+        player = InvaderPlayer()
+        fleets.append(player)
+        player.begin_interactions(fleets)
+        player.attempt_firing(fleets)
+        assert len(fi.player_shots) == 1
+        shot = fi.player_shots[0]
+        player.begin_interactions(fleets)
+        player.interact_with_playershot(shot, fleets)
+        player.attempt_firing(fleets)
+        assert len(fi.player_shots) == 1
+
