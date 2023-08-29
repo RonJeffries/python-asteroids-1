0f33469196bd31a5a743cea5ea3d3cd17704d49c
Ron Jeffries <ronjeffries@acm.org>
2023-08-29 07:14:40 -0400

    TodoList keeps list and iterates it.

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -2,6 +2,17 @@ from todolist import TodoList
 
 
 class TestReminders:
+    def __init__(self):
+        self.value = None
+
+    def save_value(self, value):
+        self.value = value
 
     def test_exists(self):
         TodoList()
+
+    def test_remembers_things(self):
+        todo = TodoList()
+        todo.remind_me(lambda: self.save_value(17))
+        todo.execute()
+        assert self.value == 17
--- todolist.py
+++ todolist.py
@@ -1,4 +1,12 @@
 
 
 class TodoList:
-    pass
+    def __init__(self):
+        self.todos = []
+        
+    def remind_me(self, func):
+        self.todos.append(func)
+
+    def execute(self):
+        for func in self.todos:
+            func()
