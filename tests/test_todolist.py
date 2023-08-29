104502ee87385a0574d2f48106fa50a1853491d2
Ron Jeffries <ronjeffries@acm.org>
2023-08-29 07:20:23 -0400

    additional test.

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -4,9 +4,13 @@ from todolist import TodoList
 class TestReminders:
     def __init__(self):
         self.value = None
+        self.total = 0
 
     def save_value(self, value):
         self.value = value
+
+    def accumulate(self, value):
+        self.total += value
 
     def test_exists(self):
         TodoList()
@@ -16,3 +20,12 @@ class TestReminders:
         todo.remind_me(lambda: self.save_value(17))
         todo.execute()
         assert self.value == 17
+
+    def test_has_list(self):
+        todo = TodoList()
+        todo.remind_me(lambda: self.save_value(86))
+        todo.remind_me(lambda: self.accumulate(13))
+        todo.remind_me(lambda: self.accumulate(13))
+        todo.execute()
+        assert self.value == 86
+        assert self.total == 26
