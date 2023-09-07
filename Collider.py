4c66cf156c62711868079d5b005dfb5cb788db67
Ron Jeffries <ronjeffries@acm.org>
2023-09-07 05:39:35 -0400

    Collider uses Masker. Unused methods and tests need cleanup.

--- Collider.py
+++ Collider.py
@@ -1,18 +1,19 @@
 from pygame import Vector2
 
+from ImageMasher import Masker
+
 
 class Collider:
     def __init__(self, left, right):
+        self.left_masker = Masker(left.mask, left.position)
+        self.right_masker = Masker(right.mask, right.position)
         self.left_rect = left.rect
         self.left_mask = left.mask
         self.right_rect = right.rect
         self.right_mask = right.mask
 
     def colliding(self):
-        if self.right_rect and self.right_rect and self.left_mask and self.right_mask:
-            return self.rectangles_colliding() and self.masks_colliding()
-        else:
-            return False
+        return self.left_masker.colliding(self.right_masker)
 
     def rectangles_colliding(self):
         return self.left_rect.colliderect(self.right_rect)
