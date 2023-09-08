eceb1f9306240710197ad7f702b1b8703b94e1c5
Ron Jeffries <ronjeffries@acm.org>
2023-09-08 08:31:23 -0400

    TimeCapsule adds flyer after time elapsed.

--- tests/test_timecapsule.py
+++ tests/test_timecapsule.py
@@ -1,3 +1,6 @@
+from fleets import Fleets
+from player_shot import PlayerShot
+from tests.tools import FI
 from timecapsule import TimeCapsule
 
 
@@ -5,3 +8,16 @@ class TestTimeCapsule:
     def test_can_create(self):
         thing = "thing"
         capsule = TimeCapsule(thing, 2)
+
+    def test_after_a_while(self):
+        crocodile = PlayerShot((100, 100))
+        a_while = 2
+        capsule = TimeCapsule(crocodile, a_while)
+        fleets = Fleets()
+        fi = FI(fleets)
+        fleets.append(capsule)
+        fleets.tick(1.1)
+        assert not fi.player_shots
+        fleets.tick(1)
+        assert fi.player_shots
+        assert fi.player_shots[0] == crocodile
--- timecapsule.py
+++ timecapsule.py
@@ -1,6 +1,19 @@
+from flyer import Flyer
 
 
-class TimeCapsule:
+class TimeCapsule(Flyer):
     def __init__(self, flyer, time):
         self.flyer = flyer
         self.time = time
+
+    def interact_with(self, other, fleets):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def tick(self, delta_time, fleets):
+        self.time -= delta_time
+        if self.time <= 0:
+            fleets.remove(self)
+            fleets.append(self.flyer)
