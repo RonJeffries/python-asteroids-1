ed855b64d71ad0effe4504d44f679fb39dc69912
Ron Jeffries <ronjeffries@acm.org>
2023-09-10 09:05:44 -0400

    PlayerMaker makes suitable time capsule in end_interactions.

--- playermaker.py
+++ playermaker.py
@@ -1,4 +1,27 @@
+from invader_player import InvaderPlayer
+from timecapsule import TimeCapsule
 
 
 class PlayerMaker:
-    pass
+    def __init__(self):
+        self.reserve = None
+        self.player_missing = True
+
+    def begin_interactions(self, _fleets):
+        self.reserve = None
+        self.player_missing = True
+
+    def interact_with_invaderplayer(self, _player, _fleets):
+        self.player_missing = False
+
+    def interact_with_reserveplayer(self, reserve, _fleets):
+        if not self.reserve:
+            self.reserve = reserve
+        elif reserve.reserve_number > self.reserve.reserve_number:
+            self.reserve = reserve
+
+    def end_interactions(self, fleets):
+        if self.player_missing and self.reserve:
+            capsule = TimeCapsule(2, InvaderPlayer(), self.reserve)
+            fleets.append(capsule)
+
--- tests/test_playermaker.py
+++ tests/test_playermaker.py
@@ -1,6 +1,40 @@
+from fleets import Fleets
+from invader_player import InvaderPlayer
 from playermaker import PlayerMaker
+from reserveplayer import ReservePlayer
+from tests.tools import FakeFleets
 
 
 class TestPlayerMaker:
     def test_exists(self):
         PlayerMaker()
+
+    def test_captures_reserve(self):
+        maker = PlayerMaker()
+        maker.interact_with_reserveplayer(ReservePlayer(1), None)
+        maker.interact_with_reserveplayer(correct :=ReservePlayer(2), None)
+        maker.interact_with_reserveplayer(ReservePlayer(0), None)
+        assert maker.reserve == correct
+        maker.begin_interactions(None)
+        assert not maker.reserve
+
+    def test_notices_player(self):
+        maker = PlayerMaker()
+        maker.begin_interactions(None)
+        assert maker.player_missing
+        maker.interact_with_invaderplayer(None, None)
+        assert not maker.player_missing
+
+    def test_rezzes_time_capsule(self):
+        maker = PlayerMaker()
+        maker.begin_interactions(None)
+        maker.interact_with_reserveplayer(ReservePlayer(0), None)
+        maker.end_interactions(fleets := FakeFleets())
+        assert fleets.appends
+        capsule = fleets.appends[0]
+        print(capsule.to_add)
+        print(capsule.to_remove)
+        assert isinstance(capsule.to_add, InvaderPlayer)
+        assert isinstance(capsule.to_remove, ReservePlayer)
+
+
