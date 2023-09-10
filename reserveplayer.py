bcf30be24ae800ed8151ab860181bf955125523f
Ron Jeffries <ronjeffries@acm.org>
2023-09-10 07:13:44 -0400

    ReservePlayer first position correct.

--- reserveplayer.py
+++ reserveplayer.py
@@ -1,5 +1,67 @@
+from pygame import Vector2
+
+import u
+from bitmap_maker import BitmapMaker
+from flyer import InvadersFlyer
 
 
-class ReservePlayer:
+class ReservePlayer(InvadersFlyer):
     def __init__(self, reserve_number):
         self.reserve_number = reserve_number
+        maker = BitmapMaker.instance()
+        players = maker.players  # one turret, two explosions
+        self.player = players[0]
+        self._rect = self.player.get_rect()
+        half_width = self.rect.width / 2
+        left = 64 + half_width
+        self.rect.center = Vector2(left, u.SCREEN_SIZE - 16)
+
+    @property
+    def mask(self):
+        return None
+
+    @property
+    def rect(self):
+        return self._rect
+
+    def draw(self, screen):
+        pass
+
+    def interact_with(self, other, fleets):
+        pass
+
+    def interact_with_bumper(self, bumper, fleets):
+        pass
+
+    def interact_with_invaderfleet(self, fleet, fleets):
+        pass
+
+    def interact_with_invaderplayer(self, player, fleets):
+        pass
+
+    def interact_with_invadershot(self, shot, fleets):
+        pass
+
+    def interact_with_playerexplosion(self, explosion, fleets):
+        pass
+
+    def interact_with_playershot(self, shot, fleets):
+        pass
+
+    def interact_with_invaderexplosion(self, explosion, fleets):
+        pass
+
+    def interact_with_shield(self, shield, fleets):
+        pass
+
+    def interact_with_shotcontroller(self, controller, fleets):
+        pass
+
+    def interact_with_shotexplosion(self, bumper, fleets):
+        pass
+
+    def interact_with_topbumper(self, top_bumper, fleets):
+        pass
+
+    def tick(self, delta_time, fleets):
+        pass
--- tests/test_reserveplayer.py
+++ tests/test_reserveplayer.py
@@ -4,3 +4,8 @@ from reserveplayer import ReservePlayer
 class TestReservePlayer:
     def test_exists(self):
         ReservePlayer(0)
+
+    def test_first_position(self):
+        player = ReservePlayer(0)
+        rect = player.rect
+        assert rect.centerx == 64 + rect.width // 2
