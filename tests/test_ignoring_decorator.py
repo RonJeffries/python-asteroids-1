ae36f20225572b28d0ba5888fd63eb90192e95e3
Ron Jeffries <ronjeffries@acm.org>
2023-09-18 06:13:06 -0400

    @ignore_decorator passes tests.

--- tests/test_ignoring_decorator.py
+++ tests/test_ignoring_decorator.py
@@ -10,10 +10,6 @@ class AbstractClass(ABC):
 
 
 class Base(ABC):
-
-    def must(self):
-        pass
-
     def foo(self):
         raise NotImplementedError
 
@@ -31,14 +27,13 @@ class Base(ABC):
 
 class ignoring_decorator(object):
     def __init__(self, names):
-        print("ig init", names, "done")
         self.names = names
 
     def __call__(self, klass):
-        print("call args", klass, "done")
         if self.names:
             for name in self.names:
-                setattr(klass, name, lambda *args: None)
+                if name not in klass.__dict__:
+                    setattr(klass, name, lambda *args: None)
         return klass
 
 
@@ -50,7 +45,7 @@ class ignoring_decorator(object):
 #     return function
 
 # @ignoring_decorator
-@ignoring_decorator(["bar"])
+@ignoring_decorator(["bar", "baz"])
 class FooDecorated(Base):
     def __init__(self):
         print("Foo init")
@@ -66,14 +61,15 @@ class TestIgnoringDecorator:
     def test_class_decorator(self):
         print("decorating")
 
-        @ignoring_decorator(None)
-        class Fubar:
-            def action(self):
-                return 4422
-        print("calling")
-        fubar = Fubar()
-        assert fubar.action() == 4422
+        # @ignoring_decorator(None)
+        # class Fubar:
+        #     def action(self):
+        #         return 4422
+        # print("calling")
+        # fubar = Fubar()
+        # assert fubar.action() == 4422
         foo = FooDecorated()
+        print("foo created")
         assert foo.foo() == "foo"
         assert foo.bar() is None
         assert foo.baz() == "baz"
