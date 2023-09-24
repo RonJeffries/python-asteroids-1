c3dd7a9d03cd0acbc31957e46a0b6bd3412f8555
Ron Jeffries <ronjeffries@acm.org>
2023-09-24 09:06:19 -0400

    clear out unneeded pass methods.

--- game_over.py
+++ game_over.py
@@ -5,34 +5,6 @@ from flyer import AsteroidFlyer
 
 
 class GameOver(AsteroidFlyer):
-
-    def interact_with_fragment(self, fragment, fleets):
-        pass
-
-    def interact_with_gameover(self, game_over, fleets):
-        pass
-
-    def interact_with_saucermaker(self, saucermaker, fleets):
-        pass
-
-    def interact_with_score(self, score, fleets):
-        pass
-
-    def interact_with_scorekeeper(self, scorekeeper, fleets):
-        pass
-
-    def interact_with_shipmaker(self, shipmaker, fleets):
-        pass
-
-    def interact_with_signal(self, signal, fleets):
-        pass
-
-    def interact_with_thumper(self, thumper, fleets):
-        pass
-
-    def interact_with_wavemaker(self, wavemaker, fleets):
-        pass
-
     def __init__(self):
         self.init_game_over()
 
@@ -62,18 +34,3 @@ class GameOver(AsteroidFlyer):
 
     def interact_with(self, other, fleets):
         other.interact_with_gameover(self, fleets)
-
-    def interact_with_asteroid(self, asteroid, fleets):
-        pass
-
-    def interact_with_missile(self, missile, fleets):
-        pass
-
-    def interact_with_saucer(self, saucer, fleets):
-        pass
-
-    def interact_with_ship(self, ship, fleets):
-        pass
-
-    def tick(self, delta_time, fleets):
-        pass
