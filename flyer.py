4313d5fd97265865e005edaf2caaa40632805655
Ron Jeffries <ronjeffries@acm.org>
2023-09-24 08:36:41 -0400

    remove almost all abstract method decorators. Leave default methods.

--- flyer.py
+++ flyer.py
@@ -4,6 +4,10 @@ from abc import ABC, abstractmethod
 
 class Flyer(ABC):
 
+    @abstractmethod
+    def interact_with(self, other, fleets):
+        pass
+
     # concrete methods, inheritable
 
     def begin_interactions(self, fleets):
@@ -12,21 +16,12 @@ class Flyer(ABC):
     def end_interactions(self, fleets):
         pass
 
-    ## abstract methods
-
-    @abstractmethod
-    def interact_with(self, other, fleets):
-        pass
-
-    @abstractmethod
     def draw(self, screen):
         pass
 
-    @abstractmethod
     def tick(self, delta_time, fleets):
         pass
 
-    # not abstract FSR
     def update(self, delta_time, fleets):
         pass
 
@@ -47,113 +42,87 @@ class InvadersFlyer(Flyer):
     def rect(self, value):
         pass
 
-    @abstractmethod
     def interact_with_bumper(self, bumper, fleets):
         pass
 
-    @abstractmethod
     def interact_with_invaderfleet(self, fleet, fleets):
         pass
 
-    @abstractmethod
     def interact_with_invaderplayer(self, player, fleets):
         pass
 
-    @abstractmethod
     def interact_with_invadershot(self, shot, fleets):
         pass
 
-    @abstractmethod
     def interact_with_playerexplosion(self, explosion, fleets):
         pass
 
-    @abstractmethod
     def interact_with_playermaker(self, maker, fleets):
         pass
 
-    @abstractmethod
     def interact_with_playershot(self, shot, fleets):
         pass
 
-    @abstractmethod
     def interact_with_invaderexplosion(self, explosion, fleets):
         pass
 
     def interact_with_reserveplayer(self, rp, fleets):
         pass
 
-    @abstractmethod
     def interact_with_shield(self, shield, fleets):
         pass
 
-    @abstractmethod
     def interact_with_shotcontroller(self, controller, fleets):
         pass
 
-    @abstractmethod
     def interact_with_shotexplosion(self, bumper, fleets):
         pass
 
-    @abstractmethod
     def interact_with_timecapsule(self, capsule, fleets):
         pass
 
-    @abstractmethod
     def interact_with_topbumper(self, top_bumper, fleets):
         pass
 
 
 class AsteroidFlyer(Flyer):
 
-    @abstractmethod
     def interact_with_asteroid(self, asteroid, fleets):
         pass
 
-    @abstractmethod
     def interact_with_fragment(self, fragment, fleets):
         pass
 
-    @abstractmethod
     def interact_with_gameover(self, game_over, fleets):
         pass
 
-    @abstractmethod
     def interact_with_missile(self, missile, fleets):
         pass
 
-    @abstractmethod
     def interact_with_saucer(self, saucer, fleets):
         pass
 
-    @abstractmethod
     def interact_with_saucermaker(self, saucermaker, fleets):
         pass
 
-    @abstractmethod
     def interact_with_score(self, score, fleets):
         pass
 
-    @abstractmethod
     def interact_with_scorekeeper(self, scorekeeper, fleets):
         pass
 
-    @abstractmethod
     def interact_with_ship(self, ship, fleets):
         pass
 
-    @abstractmethod
     def interact_with_shipmaker(self, shipmaker, fleets):
         pass
 
-    @abstractmethod
     def interact_with_signal(self, signal, fleets):
         pass
 
-    @abstractmethod
     def interact_with_thumper(self, thumper, fleets):
         pass
 
-    @abstractmethod
     def interact_with_wavemaker(self, wavemaker, fleets):
         pass
 
