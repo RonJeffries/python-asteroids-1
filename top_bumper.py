09a868907ea5368205b5006fb580f71b56481eac
Ron Jeffries <ronjeffries@acm.org>
2023-09-24 08:46:09 -0400

    clear out unneeded pass methods.

--- top_bumper.py
+++ top_bumper.py
@@ -2,12 +2,6 @@ from flyer import InvadersFlyer
 
 
 class TopBumper(InvadersFlyer):
-    def interact_with_playermaker(self, maker, fleets):
-        pass
-
-    def interact_with_timecapsule(self, capsule, fleets):
-        pass
-
     def __init__(self):
         self.y = 40
 
@@ -19,49 +13,10 @@ class TopBumper(InvadersFlyer):
     def rect(self):
         return None
 
-    def intersecting(self, point):
-        return point.y <= self.y
-
-    def interact_with_bumper(self, bumper, fleets):
-        pass
-
-    def interact_with_invaderexplosion(self, explosion, fleets):
-        pass
-
-    def interact_with_invaderfleet(self, bumper, fleets):
-        pass
-
-    def interact_with_invaderplayer(self, bumper, fleets):
-        pass
-
-    def interact_with_invadershot(self, bumper, fleets):
-        pass
-
-    def interact_with_playerexplosion(self, _explosion, _fleets):
-        pass
-
-    def interact_with_playershot(self, bumper, fleets):
-        pass
-
-    def interact_with_shield(self, shield, fleets):
-        pass
-
-    def interact_with_shotcontroller(self, controller, fleets):
-        pass
-
-    def interact_with_shotexplosion(self, bumper, fleets):
-        pass
-
-    def interact_with_topbumper(self, top_bumper, fleets):
-        pass
-
     def interact_with(self, other, fleets):
         other.interact_with_topbumper(self, fleets)
 
-    def draw(self, screen):
-        pass
-
-    def tick(self, delta_time, fleets):
-        pass
+    def intersecting(self, point):
+        return point.y <= self.y
 
 
