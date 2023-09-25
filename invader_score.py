8ca2a6a853b881bb0512684acf46d1fbc248a91d
Ron Jeffries <ronjeffries@acm.org>
2023-09-25 08:32:10 -0400

    Score<1> drawn top left.

--- invader_score.py
+++ invader_score.py
@@ -1,3 +1,5 @@
+import pygame
+
 from flyer import InvadersFlyer
 
 
@@ -23,6 +25,8 @@ class InvaderScore(InvadersFlyer):
 class InvaderScoreKeeper(InvadersFlyer):
     def __init__(self):
         self.total_score = 0
+        if pygame.get_init():
+            self.score_font = pygame.font.SysFont("andale mono", 48)
 
     @property
     def mask(self):
@@ -31,6 +35,14 @@ class InvaderScoreKeeper(InvadersFlyer):
     @property
     def rect(self):
         return None
+
+    def draw(self, screen):
+        header = "SCORE<1>"
+        header_surface = self.score_font.render(header, True, "white")
+        screen.blit(header_surface, (75, 10))
+        score_text = f"0000{self.total_score}"[-5:]
+        score_surface = self.score_font.render(score_text, True, "white")
+        screen.blit(score_surface, (135, 60))
 
     def interact_with(self, other, fleets):
         other.interact_with_invaderscorekeeper(self, fleets)
