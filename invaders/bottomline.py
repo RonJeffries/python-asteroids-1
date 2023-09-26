2565dda9745e275e7ca47e3312e1eb609a26e7af
Ron Jeffries <ronjeffries@acm.org>
2023-09-26 14:22:46 -0400

    bottomline has bitmap and mask.

--- invaders/bottomline.py
+++ invaders/bottomline.py
@@ -1,23 +1,33 @@
-import pygame.draw_py
-from pygame import Rect
 
-import u
 from flyer import InvadersFlyer
+from pygame import Rect, Surface
+import pygame.draw_py
+import u
 
 
 class BottomLine(InvadersFlyer):
+    def __init__(self):
+        w = 960-128
+        h = 4
+        surface = Surface((w, h))
+        surface.set_colorkey((0, 0, 0))
+        surface.fill("green")
+        rect = Rect(0, 0, w, h)
+        rect.bottomleft = (64, u.SCREEN_SIZE - 56)
+        self._rect = rect
+        self.map = surface
+        self._mask = pygame.mask.from_surface(surface)
 
     @property
     def mask(self):
-        return None
+        return self._mask
 
     @property
     def rect(self):
-        y = u.SCREEN_SIZE - 56
-        return Rect(64, y, 960-64, 4)
+        return self._rect
 
     def draw(self, screen):
-        pygame.draw.rect(screen, "green", self.rect)
+        screen.blit(self.map, self.rect)
 
     def interact_with(self, other, fleets):
         other.interact_with_bottomline(self, fleets)
