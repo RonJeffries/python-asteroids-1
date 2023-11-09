1ee068c5cec760351833c1c6548bf3d5d9ade757
Ron Jeffries <ronjeffries@acm.org>
2023-11-09 08:35:31 -0500

    reorder methods

--- invaders/invaders_saucer.py
+++ invaders/invaders_saucer.py
@@ -9,6 +9,7 @@ from invaders.shot_explosion import InvadersExplosion
 from invaders.timecapsule import TimeCapsule
 
 
+# noinspection PyProtectedMember
 class Unready:
     def __init__(self, saucer):
         self._saucer = saucer
@@ -36,9 +37,11 @@ class Ready:
     def finish_initializing(self, shot_count):
         pass
 
+    # noinspection PyProtectedMember
     def just_draw(self, screen):
         self._saucer._just_draw(screen)
 
+    # noinspection PyProtectedMember
     def move_or_die(self, fleets):
         self._saucer._move_or_die(fleets)
 
@@ -79,13 +82,30 @@ class InvadersSaucer(InvadersFlyer):
     def interact_with_invaderfleet(self, invader_fleet, fleets):
         self._readiness.die_if_lonely(invader_fleet, fleets)
 
-    def _die_if_lonely(self, invader_fleet, fleets):
-        if invader_fleet.invader_count() < 8:
-            self._die(fleets)
-
     def interact_with_invaderplayer(self, player, fleets):
         self._player_shot_count = player.shot_count
         self._readiness.finish_initializing(self._player_shot_count)
+
+    def interact_with_playershot(self, shot, fleets):
+        if Collider(self, shot).colliding():
+            explosion = InvadersExplosion.saucer_explosion(self.position, 0.5)
+            fleets.append(explosion)
+            fleets.append(InvaderScore(self._mystery_score()))
+            self._die(fleets)
+
+    def update(self, delta_time, fleets):
+        self._readiness.move_or_die(fleets)
+
+    def draw(self, screen):
+        self._readiness.just_draw(screen)
+
+    def _die(self, fleets):
+        fleets.remove(self)
+        fleets.append(TimeCapsule(10, InvadersSaucer()))
+
+    def _die_if_lonely(self, invader_fleet, fleets):
+        if invader_fleet.invader_count() < 8:
+            self._die(fleets)
 
     def _finish_initializing(self, shot_count):
         self._readiness = Ready(self)
@@ -95,23 +115,8 @@ class InvadersSaucer(InvadersFlyer):
         left_or_right = (self._right, self._left)[even_or_odd]
         self.rect.center = Vector2(left_or_right, u.INVADER_SAUCER_Y)
 
-    def interact_with_playershot(self, shot, fleets):
-        if Collider(self, shot).colliding():
-            explosion = InvadersExplosion.saucer_explosion(self.position, 0.5)
-            fleets.append(explosion)
-            fleets.append(InvaderScore(self._mystery_score()))
-            self._die(fleets)
-
-    def _mystery_score(self):
-        score_index = self._player_shot_count % len(self._score_list)
-        return self._score_list[score_index]
-
-    def _die(self, fleets):
-        fleets.remove(self)
-        fleets.append(TimeCapsule(10, InvadersSaucer()))
-
-    def update(self, delta_time, fleets):
-        self._readiness.move_or_die(fleets)
+    def _just_draw(self, screen):
+        screen.blit(self._map, self.rect)
 
     def _move_or_die(self, fleets):
         x = self.position.x + self._speed
@@ -120,9 +125,7 @@ class InvadersSaucer(InvadersFlyer):
         else:
             self.position = (x, self.position.y)
 
-    def draw(self, screen):
-        self._readiness.just_draw(screen)
-
-    def _just_draw(self, screen):
-        screen.blit(self._map, self.rect)
+    def _mystery_score(self):
+        score_index = self._player_shot_count % len(self._score_list)
+        return self._score_list[score_index]
 
