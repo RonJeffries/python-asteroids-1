c66ad1b26aaf307bacd4ab16f1cfee7674305112
Ron Jeffries <ronjeffries@acm.org>
2023-11-30 09:03:38 -0500

    makes masks.

--- tests/test_sprite.py
+++ tests/test_sprite.py
@@ -1,9 +1,18 @@
+import pygame
+from pygame import Mask
+
 from invaders.bitmap_maker import BitmapMaker
 
 
 class Sprite:
     def __init__(self, surfaces):
         self._surfaces = surfaces
+        self._masks = [pygame.mask.from_surface(surface) for surface in self._surfaces]
+        self._rectangle = self._surfaces[0].get_rect()
+
+    @property
+    def rectangle(self):
+        return self._rectangle
 
 
 class TestSprite:
@@ -11,3 +20,5 @@ class TestSprite:
         maps = BitmapMaker.instance().squiggles
         squiggles = Sprite(maps)
         assert squiggles._surfaces == maps
+        assert squiggles.rectangle == pygame.Rect(0, 0, 12, 32)
+        assert isinstance(squiggles._masks[0], Mask)
