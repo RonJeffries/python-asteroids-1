a15f3d9ee16d2dff4f23457e37230671c162d7c5
Ron Jeffries <ronjeffries@acm.org>
2023-11-30 15:34:25 -0500

    explode scream and die

--- invaders/invaders_saucer.py
+++ invaders/invaders_saucer.py
@@ -58,11 +58,14 @@ class InvadersSaucer(InvadersFlyer):
 
     def interact_with_playershot(self, shot, fleets):
         if Collider(self, shot).colliding():
-            explosion = InvadersExplosion.saucer_explosion(self.position, 0.5)
-            fleets.append(explosion)
-            fleets.append(InvaderScore(self._mystery_score()))
-            self.play_death_sound()
-            self._die(fleets)
+            self.explode_scream_and_die(fleets)
+
+    def explode_scream_and_die(self, fleets):
+        explosion = InvadersExplosion.saucer_explosion(self.position, 0.5)
+        fleets.append(explosion)
+        fleets.append(InvaderScore(self._mystery_score()))
+        self.play_death_sound()
+        self._die(fleets)
 
     def update(self, delta_time, fleets):
         self._move_along_x()
