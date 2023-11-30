b6872e5b4d4f59ccf2315abcc1688e5380337b9a
Ron Jeffries <ronjeffries@acm.org>
2023-11-30 08:49:02 -0500

    Sprite accepts map collection.

--- tests/test_sprite.py
+++ tests/test_sprite.py
@@ -1,9 +1,13 @@
+from invaders.bitmap_maker import BitmapMaker
 
 
 class Sprite:
-    pass
+    def __init__(self, surfaces):
+        self._surfaces = surfaces
 
 
 class TestSprite:
     def test_exists(self):
-        Sprite()
+        maps = BitmapMaker.instance().squiggles
+        squiggles = Sprite(maps)
+        assert squiggles._surfaces == maps
