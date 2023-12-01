cb04e4fd44c386c3cdcddd0ad3e4d94cd16117ff
Ron Jeffries <ronjeffries@acm.org>
2023-12-01 09:58:15 -0500

    Invader uses Sprite.

--- invaders/invader.py
+++ invaders/invader.py
@@ -5,6 +5,7 @@ import u
 from invaders.Collider import Collider
 from invaders.invader_explosion import InvaderExplosion
 from invaders.invader_score import InvaderScore
+from invaders.sprite import Sprite
 from sounds import player
 
 INVADER_SPACING = 64
@@ -13,16 +14,27 @@ INVADER_SPACING = 64
 class Invader:
     def __init__(self, column, row, bitmaps):
         self._score = [10, 10, 20, 20, 30][row]
-        self.bitmaps = bitmaps
-        self.masks = [pygame.mask.from_surface(bitmap) for bitmap in self.bitmaps]
+        self._sprite = Sprite(bitmaps)
         self.column = column
         self.relative_position = Vector2(INVADER_SPACING * column, -INVADER_SPACING * row)
-        self.rect = pygame.Rect(0, 0, 64, 32)
         self.image = 0
 
     @property
     def mask(self):
-        return self.masks[self.image]
+        return self._sprite.mask
+
+    @property
+    def rect(self):
+        return self._sprite.rectangle
+
+    @property
+    def position(self):
+        return self._sprite.position
+
+    @position.setter
+    def position(self, vector):
+        self._sprite.next_frame()
+        self.rect.center = vector + self.relative_position
 
     def interact_with_group_and_playershot(self, shot, group, fleets):
         if self.colliding(shot):
@@ -36,18 +48,8 @@ class Invader:
         collider = Collider(self, invaders_flyer)
         return collider.colliding()
 
-    @property
-    def position(self):
-        return Vector2(self.rect.center)
-
-    @position.setter
-    def position(self, vector):
-        self.image = 1 if self.image == 0 else 0
-        self.rect.center = vector + self.relative_position
-
     def draw(self, screen):
-        if screen:
-            screen.blit(self.bitmaps[self.image], self.rect)
+        self._sprite.draw(screen)
             # circle_color = "green" if self.relative_position == Vector2(0, 0) else "red"
             # pygame.draw.rect(screen, "yellow", self.rect)
             # pygame.draw.circle(screen, circle_color, self.rect.center, 16)
