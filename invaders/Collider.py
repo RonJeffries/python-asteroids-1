57f9cf9eabdd08c33f0f0cbf9b81786c88850fa5
Ron Jeffries <ronjeffries@acm.org>
2023-12-03 10:27:20 -0500

    Collider uses Sprites for collision if both objects have _sprite.

--- invaders/Collider.py
+++ invaders/Collider.py
@@ -6,12 +6,12 @@ from invaders.ImageMasher import Masker
 class Collider:
     def __init__(self, left, right):
         try:
-            self.left_masker = left._sprite
-            self.right_masker = right._sprite
+            self.left_collider = left._sprite
+            self.right_collider = right._sprite
         except AttributeError:
-            self.left_masker = Masker(left.mask, left.position)
-            self.right_masker = Masker(right.mask, right.position)
+            self.left_collider = Masker(left.mask, left.position)
+            self.right_collider = Masker(right.mask, right.position)
 
     def colliding(self):
-        return self.left_masker.colliding(self.right_masker)
+        return self.left_collider.colliding(self.right_collider)
 
