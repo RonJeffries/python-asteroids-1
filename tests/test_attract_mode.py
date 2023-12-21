fa1d5ae43356bb966007dfe41318780499b00404
Ron Jeffries <ronjeffries@acm.org>
2023-12-21 09:35:16 -0500

    better nearest.

--- tests/test_attract_mode.py
+++ tests/test_attract_mode.py
@@ -17,14 +17,22 @@ class Driver(Spritely, InvadersFlyer):
         self.position = Vector2(self.left, u.INVADER_PLAYER_Y)
         self.count = 0
 
-    def nearest(self, position_vectors):
-        best = position_vectors[0]
-        best_dist = self.x_distance(position_vectors[0])
-        for position in position_vectors:
-            dist = self.x_distance(position)
-            if dist < best_dist:
-                best = position
-                best_dist = dist
+    shield_locations = ((198, 286), (378, 466), (558, 646), (738, 826))
+    open_locations = (range(0, 198), range(287, 378), range(467, 558), range(647, 738), range(827, 1024))
+
+    def is_in_open(self, x):
+        return any([x in r for r in self.open_locations])
+
+    def select_values_in_open(self, x_values):
+        in_any_open = [x for x in x_values if self.is_in_open(x)]
+        return in_any_open
+
+    def nearest(self, x_values):
+        possibles = self.select_values_in_open(x_values)
+        best = possibles[0]
+        for x in possibles:
+            if abs(self.position.x - x) < abs(self.position.x - best):
+                best = x
         return best
 
     def x_distance(self, position_vector):
@@ -32,9 +40,6 @@ class Driver(Spritely, InvadersFlyer):
 
     def interact_with(self, other, fleets):
         other.interact_with_driver(self, fleets)
-
-    shield_locations = ((198, 286), (378, 466), (558, 646), (738, 826))
-    open_locations = ((0, 197), (287, 377), (467, 557), (647, 737), (827, 1024))
 
     def update(self, delta_time, fleets):
         if self._sprite.centerx < (378+286) / 2:
@@ -59,13 +64,22 @@ class TestAttractMode:
         assert isinstance(result, EmptyCastResult)
 
     # open_locations = ((0, 197), (287, 377), (467, 557), (647, 737), (827, 1024))
-    def test_nearest_open_x(self):
+    def test_find_open_xs(self):
         driver = Driver()
         driver.position = (500, u.INVADER_PLAYER_Y)
         x_in = (190, 290, 490, 690, 890)
         x_out = (250, 390, 590, 790)
         x_values = x_in + x_out
-        y = 512
-        invader_positions = [Vector2(x,y) for x in x_values]
-        nearest_invader_position = driver.nearest(invader_positions)
-        assert nearest_invader_position.x == 490
+        open_values = driver.select_values_in_open(x_values)
+        assert len(open_values) == len(x_in)
+        assert all([x in x_in for x in open_values])
+
+    def test_find_nearest_open_x(self):
+        driver = Driver()
+        driver.position = (500, u.INVADER_PLAYER_Y)
+        x_in = (190, 290, 490, 690, 890)
+        x_out = (250, 390, 590, 790)
+        x_values = x_in + x_out
+        nearest_invader_x = driver.nearest(x_values)
+        assert nearest_invader_x == 490
+
