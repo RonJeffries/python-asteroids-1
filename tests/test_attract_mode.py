ebc53541f98f6ed0f9c134d1bdf67e2d481cf87b
Ron Jeffries <ronjeffries@acm.org>
2023-12-21 08:57:30 -0500

    Driver can find nearest position.

--- tests/test_attract_mode.py
+++ tests/test_attract_mode.py
@@ -17,10 +17,24 @@ class Driver(Spritely, InvadersFlyer):
         self.position = Vector2(self.left, u.INVADER_PLAYER_Y)
         self.count = 0
 
+    def nearest(self, position_vectors):
+        best = position_vectors[0]
+        best_dist = self.x_distance(position_vectors[0])
+        for position in position_vectors:
+            dist = self.x_distance(position)
+            if dist < best_dist:
+                best = position
+                best_dist = dist
+        return best
+
+    def x_distance(self, position_vector):
+        return abs(self.position.x - position_vector.x)
+
     def interact_with(self, other, fleets):
         other.interact_with_driver(self, fleets)
 
     shield_locations = ((198, 286), (378, 466), (558, 646), (738, 826))
+    open_locations = ((0, 197), (287, 377), (467, 557), (647, 737), (827, 1024))
 
     def update(self, delta_time, fleets):
         if self._sprite.centerx < (378+286) / 2:
@@ -44,5 +58,14 @@ class TestAttractMode:
         result = caster.cast(x)
         assert isinstance(result, EmptyCastResult)
 
-    def test_driver(self):
+    # open_locations = ((0, 197), (287, 377), (467, 557), (647, 737), (827, 1024))
+    def test_nearest_open_x(self):
         driver = Driver()
+        driver.position = (500, u.INVADER_PLAYER_Y)
+        x_in = (190, 290, 490, 690, 890)
+        x_out = (250, 390, 590, 790)
+        x_values = x_in + x_out
+        y = 512
+        invader_positions = [Vector2(x,y) for x in x_values]
+        nearest_invader_position = driver.nearest(invader_positions)
+        assert nearest_invader_position.x == 490
