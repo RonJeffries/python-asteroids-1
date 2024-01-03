b63de8056bf40291b1ac2f9ff3be6d1c596d23cc
Ron Jeffries <ronjeffries@acm.org>
2024-01-03 09:20:40 -0500

    GenericExplosion can support a list of images displayed randomly. Not yet used.

--- invaders/generic_explosion.py
+++ invaders/generic_explosion.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 from invaders.bitmap_maker import BitmapMaker
@@ -10,34 +12,26 @@ class GenericExplosion(InvadersFlyer):
     def invader_explosion(cls, position, time):
         maker = BitmapMaker()
         image = maker.invader_explosion
-        return cls(image, position, time)
+        return cls((image,), position, time)
 
     @classmethod
     def saucer_explosion(cls, position, time):
         maker = BitmapMaker()
         image = maker.saucer_explosion
-        return cls(image, position, time)
+        return cls((image,), position, time)
 
     @classmethod
     def shot_explosion(cls, position, time):
         maker = BitmapMaker()
         image = maker.player_shot_explosion
-        return cls(image, position, time)
+        return cls((image,), position, time)
 
-    def __init__(self, image, position, time):
-        self.image = image
+    def __init__(self, images, position, time):
+        self.images = images
         self.position = position
-        self._mask = pygame.mask.from_surface(image)
-        self._rect = image.get_rect()
+        self._mask = pygame.mask.from_surface(images[0])
+        self._rect = images[0].get_rect()
         self._time = time
-
-    @property
-    def mask(self):
-        return self._mask
-
-    @property
-    def rect(self):
-        return self._rect
 
     def tick(self, delta_time, fleets):
         self._time -= delta_time
@@ -46,7 +40,15 @@ class GenericExplosion(InvadersFlyer):
 
     def draw(self, screen):
         self.rect.center = self.position
-        screen.blit(self.image, self.rect)
+        screen.blit(random.choice(self.images), self.rect)
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @property
+    def rect(self):
+        return self._rect
 
     def interact_with(self, other, fleets):
         other.interact_with_genericexplosion(self, fleets)
