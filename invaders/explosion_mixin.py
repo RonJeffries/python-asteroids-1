f24964089bb8091045c649b451c52c57631c50c3
Ron Jeffries <ronjeffries@acm.org>
2024-01-06 08:49:05 -0500

    making duplication.

--- invaders/explosion_mixin.py
+++ invaders/explosion_mixin.py
@@ -5,15 +5,17 @@ import u
 
 class ExplosionMixin:
     def explode_player(self, fleets):
-        frac = u.screen_fraction(self.position)
-        player.play_stereo("explosion", frac)
+        player_explosion_sound = "explosion"
         explosion = GenericExplosion.player_explosion(self.position, 1.0)
+        frac = u.screen_fraction(self.position)
+        player.play_stereo(player_explosion_sound, frac)
         fleets.append(explosion)
         fleets.remove(self)
 
     def explode_saucer(self, fleets):
-        frac = u.screen_fraction(self.position)
-        player.play_stereo("ufo_highpitch", frac, True)
+        saucer_explosion_sound = "ufo_highpitch"
         explosion = GenericExplosion.saucer_explosion(self.position, 0.5)
+        frac = u.screen_fraction(self.position)
+        player.play_stereo(saucer_explosion_sound, frac)
         fleets.append(explosion)
         fleets.remove(self)
