a1b85121f23a066b635cdc9e5bb01d0a3b51c798
Ron Jeffries <ronjeffries@acm.org>
2024-01-06 08:53:49 -0500

    remove duplication via ExtractMethod.

--- invaders/explosion_mixin.py
+++ invaders/explosion_mixin.py
@@ -7,15 +7,15 @@ class ExplosionMixin:
     def explode_player(self, fleets):
         player_explosion_sound = "explosion"
         explosion = GenericExplosion.player_explosion(self.position, 1.0)
-        frac = u.screen_fraction(self.position)
-        player.play_stereo(player_explosion_sound, frac)
-        fleets.append(explosion)
-        fleets.remove(self)
+        self.make_the_explosion(player_explosion_sound, explosion, fleets)
 
     def explode_saucer(self, fleets):
         saucer_explosion_sound = "ufo_highpitch"
         explosion = GenericExplosion.saucer_explosion(self.position, 0.5)
+        self.make_the_explosion(saucer_explosion_sound, explosion, fleets)
+
+    def make_the_explosion(self, player_explosion_sound, explosion, fleets):
         frac = u.screen_fraction(self.position)
-        player.play_stereo(saucer_explosion_sound, frac)
+        player.play_stereo(player_explosion_sound, frac)
         fleets.append(explosion)
         fleets.remove(self)
