01471fc12ba9afda3a037dd80286e6510d7c41dc
Ron Jeffries <ronjeffries@acm.org>
2024-01-08 12:59:24 -0500

    Exploder is all class-side code. Questionable?

--- invaders/exploder.py
+++ invaders/exploder.py
@@ -5,25 +5,25 @@ import u
 
 class Exploder():
     @classmethod
-    def explode_invader(self, position, fleets):
+    def explode_invader(cls, position, fleets):
         invader_explosion_sound = "invaderkilled"
         explosion = GenericExplosion.invader_explosion(position, 0.125)
-        Exploder().explode(position, invader_explosion_sound, explosion, fleets)
+        cls.explode(position, invader_explosion_sound, explosion, fleets)
 
     @classmethod
-    def explode_player(self, position, fleets):
+    def explode_player(cls, position, fleets):
         player_explosion_sound = "explosion"
         explosion = GenericExplosion.player_explosion(position, 1.0)
-        Exploder().explode(position, player_explosion_sound, explosion, fleets)
+        cls.explode(position, player_explosion_sound, explosion, fleets)
 
     @classmethod
-    def explode_saucer(self, position, fleets):
+    def explode_saucer(cls, position, fleets):
         saucer_explosion_sound = "ufo_highpitch"
         explosion = GenericExplosion.saucer_explosion(position, 0.5)
-        Exploder().explode(position, saucer_explosion_sound, explosion, fleets)
+        cls.explode(position, saucer_explosion_sound, explosion, fleets)
 
-    @staticmethod
-    def explode(position, sound, explosion, fleets):
+    @classmethod
+    def explode(cls, position, sound, explosion, fleets):
         frac = u.screen_fraction(position)
         player.play_stereo(sound, frac)
         fleets.append(explosion)
