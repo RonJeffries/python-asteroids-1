0ccc9dccde89e4a4d4de450ca945231236297c10
Ron Jeffries <ronjeffries@acm.org>
2024-01-15 13:40:59 -0500

    correctly compute delay as f(number_of_aliens)

--- tests/test_tone_player.py
+++ tests/test_tone_player.py
@@ -1,3 +1,5 @@
+import pytest
+
 from invaders.tone_player import TonePlayer
 
 
@@ -12,3 +14,13 @@ class TestTonePlayer:
         assert player.next_tone() == "fastinvader2"
         assert player.next_tone() == "fastinvader3"
         assert player.next_tone() == "fastinvader4"
+
+    def test_delay(self):
+        player = TonePlayer()
+        assert player.delay(55) == 0x34
+
+    @pytest.mark.parametrize("invaders,delay", [(55, 0x34), (49, 0x2E), (0x11, 0x18), (0, 0x05), (-8, 0x05)])
+    def test_delays(self, invaders, delay):
+        player = TonePlayer()
+        assert player.delay(invaders) == delay
+
