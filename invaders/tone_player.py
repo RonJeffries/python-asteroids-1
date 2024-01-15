2caa87a3577ae27505525f900a8b3699d3087764
Ron Jeffries <ronjeffries@acm.org>
2024-01-15 10:39:37 -0500

    TonePlayer cycles tones.

--- invaders/tone_player.py
+++ invaders/tone_player.py
@@ -1,5 +1,10 @@
 
 
 class TonePlayer:
+    def __init__(self):
+        self._tones = ("fastinvader4", "fastinvader1", "fastinvader2", "fastinvader3")
+        self._tone_index = 3
+        
     def next_tone(self):
-        return "fastinvader4"
+        self._tone_index = (self._tone_index + 1) % 4
+        return self._tones[self._tone_index]
--- tests/test_tone_player.py
+++ tests/test_tone_player.py
@@ -8,3 +8,7 @@ class TestTonePlayer:
     def test_tone_order(self):
         player = TonePlayer()
         assert player.next_tone() == "fastinvader4"
+        assert player.next_tone() == "fastinvader1"
+        assert player.next_tone() == "fastinvader2"
+        assert player.next_tone() == "fastinvader3"
+        assert player.next_tone() == "fastinvader4"
